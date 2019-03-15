7d287e1472beb858f546247fad9cfef0461b0c31
BarabasiB <barabasibarna@gmail.com>
2019-03-15 16:12:26 +0200

    Reading and printing the state

--- AStar-SlidingPuzzle/AStar-SlidingPuzzle/Program.cs
+++ AStar-SlidingPuzzle/AStar-SlidingPuzzle/Program.cs
@@ -10,6 +10,27 @@ namespace AStar_SlidingPuzzle
     {
         static void Main(string[] args)
         {
+            char[] matrix = new char[9];
+            string numbers = string.Empty;
+            Console.Write("Enter the starting state in one line with no spaces between the numbers: ");
+            Console.WriteLine();
+            numbers = Console.ReadLine();
+            matrix = numbers.ToArray();
+            PrintMatrix(matrix);
+            Console.ReadKey();
+        }
+
+        private static void PrintMatrix(char[] matrix)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write(matrix[i]);
+                Console.Write(" ");
+                if ((i + 1) % 3 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
